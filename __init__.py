39858b60563cf4284e8c5991ecc6c1623faa14fd
BkUser <hhhhhhhhnn70@gmail.com>
2017-05-21 15:21:10 +0300

    finds most common words

--- __init__.py
+++ __init__.py
@@ -1,22 +1,54 @@
+import os
 import re
 from cudatext import *
+from collections import Counter
+
+COMMON_COUNT = 30
+REPORT = """Text Statistics for "%s"
+
+Lines: %d
+Words: %d
+Letters: %d
+All chars: %d
+
+Most common words (%d):
+%s
+"""
+
 
 def count_words(s):
     return len(re.findall(r'\w+', s))
-    
-def count_letters(s):    
+
+def count_letters(s):
     return len(re.findall(r'\w', s))
+
+def get_common_words(s):
+    words = re.findall(r'\w{2,}', s)
+    words = [w.lower() for w in words]
+    a = Counter(words)
+    res = a.most_common()[:COMMON_COUNT]
+    res = ['%s (%d)'%(item[0], item[1]) for item in res]
+    return ', '.join(res)
 
 
 class Command:
     def run(self):
         s = ed.get_text_all()
-        
+
         n_words = count_words(s)
         n_letters = count_letters(s)
         n_lines = ed.get_line_count()
         n_chars = sum([len(ed.get_text_line(i)) for i in range(ed.get_line_count())])
-        
-        text = 'Statistics:\n\nLines: %d\nWords: %d\nLetters: %d\nAll chars: %d' % \
-               (n_lines, n_words, n_letters, n_chars)
-        msg_box(text, MB_OK+MB_ICONINFO)
+
+        common = get_common_words(s)
+        text = REPORT % (
+            os.path.basename(ed.get_filename()),
+            n_lines, n_words, n_letters, n_chars,
+            COMMON_COUNT,
+            common
+            )
+
+        res = msg_box(text+'\nShow report in a new tab?', MB_OKCANCEL+MB_ICONINFO)
+        if res==ID_OK:
+            file_open('')
+            ed.set_text_all(text)
---
+++ readme/readme.txt
@@ -0,0 +1,13 @@
+plugin for CudaText.
+gets statistics for text in the current editor tab.
+and shows it in a message-box, allowing to output report to a new editor tab.
+gets info:
+
+- count of lines
+- count of all chars/ letter chars
+- count of words (with alpha-numerical chars)
+- most common words (up to 30, can change this count in source code)
+
+
+author: Alexey T.
+license: MIT
